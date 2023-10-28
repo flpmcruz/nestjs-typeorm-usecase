a695d9241563c99fdacf40e868c30b7e9d81bb01
Felipe <flpmireles@gmail.com>
2023-10-28 09:57:32 -0500

    adding boilerplate axios adaptar class to common module

---
+++ src/common/adapters/axios.adapter.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+
+interface HttpAdapter {
+  get<T>(url: string): Promise<T>;
+}
+
+@Injectable()
+export class AxiosAdapter implements HttpAdapter {
+  private axios: AxiosInstance = axios;
+
+  async get<T>(url: string): Promise<T> {
+    try {
+      const { data } = await this.axios.get<T>(url);
+      return data;
+    } catch (error) {
+      throw new Error('This is an error - Check logs');
+    }
+  }
+}
